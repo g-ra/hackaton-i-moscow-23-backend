63f50d355a813c7edb90cd46f951b3f96b1f4c64
g-ra <gds@nxt.ru>
2024-06-16 22:59:42 +0300

    video

--- src/resources/views/video/video.blade.php
+++ src/resources/views/video/video.blade.php
@@ -5,13 +5,11 @@
     <!-- <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script> -->
 </head>
 @if(!empty($video))
+
     <video
-
         controls
-
         width="99%"
         height="400px"
-
     >
         <source src="http://localhost:8181/stream-video?videoName={{ $video->path }}" />
         <p class="vjs-no-js">
@@ -22,5 +20,15 @@
             >
         </p>
     </video>
+    <div id="video-controls">
+        @php
+            $times = array_column(json_decode($video->path, true), 'time');
+            $uniqueTimes = array_unique($times);
+            sort($uniqueTimes);
+        @endphp
+        @foreach($uniqueTimes as $time)
+            <button onclick="seekTo({{ $time }})">{{ gmdate("H:i:s", $time) }}</button>
+        @endforeach
+    </div>
 @endif
 
