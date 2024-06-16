936eb1421fbba32c4469acd685db5b4510d90d09
AleksKravtsov <sanksland@gmail.com>
2024-06-16 22:12:13 +0300

    Uploaded

--- AI-server/photos_predict.py
+++ AI-server/photos_predict.py
@@ -2,11 +2,14 @@ from ultralytics import YOLOv10
 import os 
 
 # Load a model
-model = YOLOv10("best_yolo10b_200it.pt")  # pretrained YOLOv8n model
+model = YOLOv10("best_yolo_v10_100_final.pt")  # pretrained YOLOv8n model
 
 DIRECTORY = "uploads/"
 SAVE_DIR = "export/"
 
+def convert_value(value):
+    conversion_dict = {3: 0, 2: 1, 1: 2, 0: 3, 4: 4}
+    return conversion_dict.get(value, "Invalid value")
 
 def write_output_file(bboxes, filename,full=True):
 
@@ -23,18 +26,18 @@ def pics_to_text():
     # Run batched inference on a list of images
     pics = os.listdir(DIRECTORY)
     for pic in pics:
-        results = model(source=DIRECTORY+pic,stream=True)  # return a list of Results objects
+        results = model(source=DIRECTORY+pic)  # return a list of Results objects
 
         # Process results list
         for result in results:
 
             if result != None:
-                boxes = result.boxes.xyxy.cpu().numpy().astype(int)  # Boxes object for bounding box outputs
+                boxes = result.boxes.xywh.cpu().numpy().astype(int)  # Boxes object for bounding box outputs
                 classes =  result.boxes.cls.cpu().numpy().astype(int)
                 bboxes = []
                 for cls, box in zip(classes,boxes):
                     x_center, y_center, width, height = box
-                    bboxes.append({'id_class': cls, 'x_center': x_center, 'y_center': y_center, 'width': width, 'height': height})
+                    bboxes.append({'id_class': convert_value(cls), 'x_center': x_center, 'y_center': y_center, 'width': width, 'height': height})
                 write_output_file(bboxes, f"{pic[:-3]}txt")  # save bounding boxes to disk
 
             else:
