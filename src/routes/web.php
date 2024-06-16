f1084fd1b8451eff8635b20f2bc6c3580bf3ce70
g-ra <gds@nxt.ru>
2024-06-16 22:18:24 +0300

    video

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('platform.video');
 });
 
 Route::get('/stream-video', [\App\Http\Controllers\VideoStreamController::class, 'streamVideo']);
